9d46e8eacc1a46a4cf331f5f0b15ec240672a6c4
OkiMaureen <oki.maureen.eloho@gmail.com>
2018-05-18 10:02:33 +0100

    [Bug #157688048] fix eslint file name and eslint issues

--- server/middleware/requestValidations.js
+++ server/middleware/requestValidations.js
@@ -6,132 +6,133 @@ import Validator from 'validator';
  * @class Validate Request
  */
 export default class validateRequest {
-	/**
-	 * validate Request input string
-	 *
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {Function} next
-	 * @return {Object}
-	 */
-	static validateString(req, res, next){
-		const {
-			title,
-			department,
-			equipment,
-			serialNumber,
-			description
-		} = req.body;
-		const error = {};
-		if (!title) {
-			error.title = 'title is required'
-		}
-		if (title && Validator.isEmpty(title.trim() || '')) {
-			error.title = 'title is required'
-		}
-		if (!department) {
-			error.department = 'department is required'
-		}
+  /**
+   * validate Request input string
+   *
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * @return {Object}
+   */
+  static validateString(req, res, next) {
+    const {
+      title,
+      department,
+      equipment,
+      serialNumber,
+      description,
+    } = req.body;
+    const error = {};
+    if (!title) {
+      error.title = 'title is required';
+    }
+    if (title && Validator.isEmpty(title.trim() || '')) {
+      error.title = 'title is required';
+    }
+    if (!department) {
+      error.department = 'department is required';
+    }
 
-		if (department && Validator.isEmpty(department.trim() || '')) {
-			error.department = 'department is required'
-		}
-		if (!equipment) {
-			error.equipment = 'equipment is required'
-		}
-		if (equipment && Validator.isEmpty(equipment.trim() || '')) {
-			error.equipment = 'equipment is required'
-		}
-		if (!serialNumber) {
-			error.serialNumber = 'serialNumber is required'
-		}
-		if (serialNumber && Validator.isEmpty(serialNumber.trim() || '')) {
-			error.serialNumber = 'serialNumber is required'
-		}
-		if (!description) {
-			error.description = 'description is required'
-		}
-		if (description && Validator.isEmpty(description.trim() || '')) {
-			error.description = 'description is required'
-		}
-		if (isEmpty(error)) return next();
-		return res.status(406).json({
-			error
-		});
-	}
-	/**
-	 * validate Request input length
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {Function} next
-	 * @return {Object}
-	 */
-	static checkLength(req, res, next){
-		const {
-			title,
-			serialNumber,
-			description
-		} = req.body;
+    if (department && Validator.isEmpty(department.trim() || '')) {
+      error.department = 'department is required';
+    }
+    if (!equipment) {
+      error.equipment = 'equipment is required';
+    }
+    if (equipment && Validator.isEmpty(equipment.trim() || '')) {
+      error.equipment = 'equipment is required';
+    }
+    if (!serialNumber) {
+      error.serialNumber = 'serialNumber is required';
+    }
+    if (serialNumber && Validator.isEmpty(serialNumber.trim() || '')) {
+      error.serialNumber = 'serialNumber is required';
+    }
+    if (!description) {
+      error.description = 'description is required';
+    }
+    if (description && Validator.isEmpty(description.trim() || '')) {
+      error.description = 'description is required';
+    }
+    if (isEmpty(error)) return next();
+    return res.status(406).json({
+      error,
+    });
+  }
+  /**
+   * validate Request input length
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * @return {Object}
+   */
+  static checkLength(req, res, next) {
+    const {
+      title,
+      serialNumber,
+      description,
+    } = req.body;
 
-		if (!Validator.isLength(title, {
-				min: 5,
-				max: 20
-			})) {
-			return res.status(406)
-			.json({
-        message: 'Title must be between 5 and 20 characters',
-        status: "fail"
-     });
-		}
-		if (!Validator.isLength(serialNumber, {
-				min: 8,
-				max: 8
-			})) {
-			return res.status(406)
-      .json({
-        message: 'SerialNumber must be only 8 characters',
-        status: "fail"
-     });
-		}
-		if (!Validator.isLength(description, {
-				min: 3,
-				max: 50
-			})) {
-			res.status(406)
-      .json({
-        message: 'Description must be between 3 and 50 characters',
-        status: "fail"
-     });
-		}
-		next();
-	}
-	/**
-	 * validate Request id parameter
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {Function} next
-	 * @return {Object}
-	 */
-	static idIsNumber(req, res, next){
-		const {
-			id
-		} = req.params;
-		const error = {};
-		if (!id) {
-			error.id = 'id is required'
-		}
-		if (id < 0) {
-			error.id = 'id is required'
-		}
-		if (id && Validator.isEmpty(id.trim() || '')) {
-			error.id = 'id is required'
-		}
-		if (Number.isNaN(parseInt(id, 10))) {
-			error.id = 'id must be a number'
-			if (isEmpty(error)) return next();
-			return res.status(400).json({
-				error
-			});
-		}
-	}
+    if (!Validator.isLength(title, {
+      min: 5,
+      max: 20,
+    })) {
+      return res.status(406)
+        .json({
+          message: 'Title must be between 5 and 20 characters',
+          status: 'fail',
+        });
+    }
+    if (!Validator.isLength(serialNumber, {
+      min: 8,
+      max: 8,
+    })) {
+      return res.status(406)
+        .json({
+          message: 'SerialNumber must be only 8 characters',
+          status: 'fail',
+        });
+    }
+    if (!Validator.isLength(description, {
+      min: 3,
+      max: 50,
+    })) {
+      return res.status(406)
+        .json({
+          message: 'Description must be between 3 and 50 characters',
+          status: 'fail',
+        });
+    }
+    return next();
+  }
+  /**
+   * validate Request id parameter
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next
+   * @return {Object} json
+   */
+  static idIsNumber(req, res, next) {
+    const {
+      id,
+    } = req.params;
+    const error = {};
+    if (!id) {
+      error.id = 'id is required';
+    }
+    if (id < 0) {
+      error.id = 'id is required';
+    }
+    if (id && Validator.isEmpty(id.trim() || '')) {
+      error.id = 'id is required';
+    }
+    if (Number.isNaN(parseInt(id, 10))) {
+      error.id = 'id must be a number';
+      if (isEmpty(error)) return next();
+      return res.status(406).json({
+        error,
+      });
+    }
+    return next();
+  }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,15 @@
 import requestsController from '../controllers/requestController';
 import validations from '../middleware/requestValidations';
 
-
 // default route
 const routes = (app) => {
-	// default route
-	app.get('/', (req, res) => {
-		res.status(200)
-			.send('Welcome to Maintenance Tracker API');
-	});
+  // default route
+  app.get('/', (req, res) => {
+    res.status(200)
+      .send('Welcome to Maintenance Tracker API');
+  });
 
-  app.use('*', validations.validateString, validations.checkLength)
-	app.post('/api/v1/users/requests', requestsController.addRequest);
-}
+  app.use('*', validations.validateString, validations.checkLength);
+  app.post('/api/v1/users/requests', requestsController.addRequest);
+};
 export default routes;
